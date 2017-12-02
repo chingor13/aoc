126995f1febdc399003c27acc78bc3e1e147282c
Jeff Ching <ching.jeff@gmail.com>
2017-12-01 21:28:05 -0800

    Day 2 part 1

--- lib/aoc2017/day2.ex
+++ lib/aoc2017/day2.ex
@@ -1,8 +1,42 @@
 defmodule Aoc2017.Day2 do
 
-  def part1(n) when is_integer(n), do: part1("#{n}")
-  def part1(n) do
+  def part1(n) when is_binary(n) do
+    n
+    |> String.split("\n")
+    |> part1
   end
+  def part1(input) when is_list(input) do
+    input
+    |> Enum.map(fn row -> checksum(row) end)
+    |> Enum.sum
+  end
+
+  def checksum(numbers) when is_binary(numbers) do
+    numbers
+    |> String.split()
+    |> Enum.map(&int_val/1)
+    |> checksum
+  end
+  def checksum([first | rest]) do
+    checksum(rest, first, first)
+  end
+  def checksum([a | rest], min, max) do
+    checksum(rest, min(a, min), max(a, max))
+  end
+  def checksum([], min, max), do: max - min
+
+  def int_val(str) do
+    str
+    |> Integer.parse
+    |> elem(0)
+  end
+  #
+  # defp min(a, min) when a < min, do: a
+  # defp min(_, min), do: min
+  #
+  # defp max(a, min) when a > min, do: a
+  # defp max(_, min), do: min
+
 
   def part2(n) when is_integer(n), do: part2("#{n}")
   def part2(n) do
--- test/aoc2017/day2_test.exs
+++ test/aoc2017/day2_test.exs
@@ -3,22 +3,33 @@ defmodule Aoc2017Test.Day2 do
   doctest Aoc2017.Day2
   @moduletag :day2
 
+  test "checksum" do
+    assert 8 == Aoc2017.Day2.checksum([5, 1, 9, 5])
+  end
+
+  test "checksum string" do
+    assert 8 == Aoc2017.Day2.checksum("5 1 9 5")
+  end
+
   test "example 1" do
-    assert 3 == Aoc2017.Day2.part1(1122)
+    assert 18 == Aoc2017.Day2.part1([
+      [5, 1, 9, 5],
+      [7, 5, 3],
+      [2, 4, 8, 8]
+    ])
   end
-
-  test "example 2" do
-    assert 4 == Aoc2017.Day2.part1(1111)
-  end
-
-  test "example 3" do
-    assert 0 == Aoc2017.Day2.part1(1234)
-  end
-
-  test "example 4" do
-    assert 9 == Aoc2017.Day2.part1(91212129)
-  end
-
+  # test "example 2" do
+  #   assert 4 == Aoc2017.Day2.part1(1111)
+  # end
+  #
+  # test "example 3" do
+  #   assert 0 == Aoc2017.Day2.part1(1234)
+  # end
+  #
+  # test "example 4" do
+  #   assert 9 == Aoc2017.Day2.part1(91212129)
+  # end
+  #
   test "part1" do
     res = "data/day2.txt"
     |> File.read!
@@ -27,33 +38,33 @@ defmodule Aoc2017Test.Day2 do
 
     IO.puts "Day 2 part 1: #{res}"
   end
-
-  test "part 2 example 1" do
-    assert 6 == Aoc2017.Day2.part2(1212)
-  end
-
-  test "part 2 example 2" do
-    assert 0 == Aoc2017.Day2.part2(1221)
-  end
-
-  test "part 2 example 3" do
-    assert 4 == Aoc2017.Day2.part2(123425)
-  end
-
-  test "part 2 example 4" do
-    assert 12 == Aoc2017.Day2.part2(123123)
-  end
-
-  test "part 2 example 5" do
-    assert 4 == Aoc2017.Day2.part2(12131415)
-  end
-
-  test "part2" do
-    res = "data/day2.txt"
-    |> File.read!
-    |> String.trim
-    |> Aoc2017.Day2.part2
-
-    IO.puts "Day 2 part 2: #{res}"
-  end
+  #
+  # test "part 2 example 1" do
+  #   assert 6 == Aoc2017.Day2.part2(1212)
+  # end
+  #
+  # test "part 2 example 2" do
+  #   assert 0 == Aoc2017.Day2.part2(1221)
+  # end
+  #
+  # test "part 2 example 3" do
+  #   assert 4 == Aoc2017.Day2.part2(123425)
+  # end
+  #
+  # test "part 2 example 4" do
+  #   assert 12 == Aoc2017.Day2.part2(123123)
+  # end
+  #
+  # test "part 2 example 5" do
+  #   assert 4 == Aoc2017.Day2.part2(12131415)
+  # end
+  #
+  # test "part2" do
+  #   res = "data/day2.txt"
+  #   |> File.read!
+  #   |> String.trim
+  #   |> Aoc2017.Day2.part2
+  #
+  #   IO.puts "Day 2 part 2: #{res}"
+  # end
 end
